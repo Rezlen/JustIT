64d872b5e280584de8f50fa5ee7a7857893144c3
rezlen <soheilyreza@yahoo.com>
2023-10-11 10:20:09 +0100

    New Folders

---
+++ CopyFrom/src/App.js
@@ -0,0 +1,14 @@
+import ParticlesBackground from "./components/ParticlesBackground";
+
+function App() {
+  return (
+    <div className="App">
+      <ParticlesBackground />
+      <div className="particlesholder">
+        <hi>Particles.js</hi>
+      </div>
+    </div>
+  );
+}
+
+export default App;
